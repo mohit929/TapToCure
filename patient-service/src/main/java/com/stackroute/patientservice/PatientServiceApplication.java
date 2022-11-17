567e4ea426d1a24188c213cd3e190645a2c951e5
Aviral <aviral.dixit@globallogic.com>
2022-11-17 13:15:31 +0530

    patient-service added

---
+++ patient-service/src/main/java/com/stackroute/patientservice/PatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientServiceApplication.class, args);
+	}
+
+}

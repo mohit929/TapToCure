2cf4900a67967d4d277666facf891bb9062f2e98
mohit suryawanshi <mohit.suryawanshi@globallogic.com>
2022-11-23 23:16:41 +0530

    Adding Jwt token Related code and implemention on the same

--- authentication-service/src/main/java/com/stackroute/authenticationservice/service/CustomUserDetailsService.java
+++ authentication-service/src/main/java/com/stackroute/authenticationservice/service/CustomUserDetailsService.java
@@ -2,6 +2,9 @@ package com.stackroute.authenticationservice.service;
 
 import com.stackroute.authenticationservice.model.User;
 import com.stackroute.authenticationservice.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.event.EventRecodingLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,16 +12,20 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     UserRepository  userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Inside CustomUserDetailService will be printing user value next from repository");
         User user=userRepository.findByUserName(username);
-        if(user!=null){
+        System.out.println(user);
         CustomUserDetails customUserDetail=new CustomUserDetails(user);
-        return customUserDetail;}
+        if(user!=null){
+        return customUserDetail;
+        }
         else{
             throw new UsernameNotFoundException("User Not found");
         }

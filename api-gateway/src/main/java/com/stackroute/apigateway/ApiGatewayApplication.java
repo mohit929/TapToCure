b3cdba1eca45e1a0f47221984baa297a51bff2d0
mohit suryawanshi <mohit.suryawanshi@globallogic.com>
2022-12-08 14:42:32 +0530

    Adding swagger dependency for api-gateway

--- api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
@@ -1,6 +1,6 @@
 package com.stackroute.apigateway;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,7 +9,6 @@ import org.springframework.http.codec.ServerCodecConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
-@OpenAPIDefinition
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
